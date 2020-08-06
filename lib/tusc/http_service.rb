6a32aa7a533119fd1d6c832d0282bb7fa738b0ab
Matt Scilipoti <matt@scilipoti.name>
2020-08-06 09:58:48 -0400

    HttpService: extract _log_request/response

--- lib/tusc/http_service.rb
+++ lib/tusc/http_service.rb
@@ -2,6 +2,7 @@ require 'digest'
 require 'net/http'
 require_relative '../core_ext/string/truncate'
 
+# Provides basic http calls (head, patch, post), with detailed logging
 class HttpService
   def self.head(uri:, headers:, logger:)
     request = Net::HTTP::Head.new(uri, headers)
@@ -20,18 +21,20 @@ class HttpService
     _perform(http_request: request, logger: logger)
   end
 
-  def self._perform(http_request:, logger:)
-    uri = http_request.uri
-
+  def self._log_request(http_request, logger)
     logger.debug do
+      uri = http_request.uri
+
       header_info = {}
       http_request.each_header do |key, value|
         header_info[key] = value
       end
+
       request_info = { uri: uri.to_s, header: header_info }
 
       request_body = http_request.body.to_s
       request_info[:body_md5] = Digest::MD5.hexdigest(request_body) unless request_body.blank?
+      request_info[:body_size] = request_body.size unless request_body.blank?
 
       formatted_body =  case request_body.encoding
                         when Encoding::ASCII_8BIT
@@ -44,7 +47,28 @@ class HttpService
       ["TUS #{http_request.method}",
        request: request_info]
     end
+  end
 
+  def self._log_response(http_method, http_response, logger)
+    header_info = {}
+    http_response.each_header do |key, value|
+      header_info[key] = value
+    end
+
+    logger.debug do
+      ["TUS #{http_method}",
+       response: {
+         status: http_response.code,
+         header: header_info,
+         body: http_response.body.to_s.truncate_middle(60)
+       }]
+    end
+  end
+
+  def self._perform(http_request:, logger:)
+    _log_request(http_request, logger)
+
+    uri = http_request.uri
     http_response = Net::HTTP.start(
       uri.host,
       uri.port,
@@ -53,17 +77,7 @@ class HttpService
       http.request http_request
     end
 
-    http_response.tap do |response|
-      received_header = response.each_key.collect { |k| { k => response.header[k] } }
-
-      logger.debug do
-        ["TUS #{http_request.method}",
-         response: {
-           status: response.code,
-           header: received_header,
-           body: response.body.to_s.truncate_middle(60)
-         }]
-      end
-    end
+    _log_response(http_request.method, http_response, logger)
+    http_response
   end
 end
