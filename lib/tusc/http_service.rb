c948842cd171d937571142ee98dc08bf43e7e3eb
Matt Scilipoti <matt@scilipoti.name>
2020-08-05 23:57:19 -0400

    HttpService uses Net::HTTP::Request objects
    - extracted common HttpService#_perform. Logging exists in one place now.

--- lib/tusc/http_service.rb
+++ lib/tusc/http_service.rb
@@ -4,85 +4,60 @@ require_relative '../core_ext/string/truncate'
 
 class HttpService
   def self.head(uri:, headers:, logger:)
-    logger.debug do
-      ['TUS HEAD',
-       request: { upload_url: uri.to_s, header: headers }]
-    end
-
-    Net::HTTP.start(
-      uri.host,
-      uri.port,
-      use_ssl: uri.scheme == 'https'
-    ) do |http|
-      http.head(uri.path, headers)
-    end.tap do |response|
-      received_header = response.each_key.collect { |k| { k => response.header[k] } }
-
-      logger.debug do
-        ['TUS HEAD',
-         response: {
-           status: response.code,
-           header: received_header,
-           body: response.body.to_s.truncate_middle(60)
-         }]
-      end
-    end
+    request = Net::HTTP::Head.new(uri, headers)
+    _perform(http_request: request, logger: logger)
   end
 
   def self.patch(uri:, headers:, body:, logger:)
-    logger.debug do
-      ['TUS PATCH',
-       request: {
-         # WORKAROUND: receiving error truncating body
-         # *** Encoding::CompatibilityError Exception: incompatible character encodings: UTF-8 and ASCII-8BIT
-         # For the test file, it works for truncate_middle(12)
-         #  but not truncate_middle(13)
-         # body: chunk.to_s.truncate_middle(50),
-         body_md5: Digest::MD5.hexdigest(body),
-         header: headers,
-         url: uri.to_s
-       }]
-    end
-
-    Net::HTTP.start(
-      uri.host,
-      uri.port,
-      use_ssl: uri.scheme == 'https'
-    ) do |http|
-      http.patch(uri.path, body, headers)
-    end.tap do |response|
-      received_header = response.each_key.collect { |k| { k => response.header[k] } }
-      logger.debug do
-        ['TUS PATCH',
-         response: {
-           status: response.code,
-           header: received_header,
-           body: response.body.to_s.truncate_middle(60)
-         }]
-      end
-    end
+    request = Net::HTTP::Patch.new(uri, headers)
+    request.body = body
+    _perform(http_request: request, logger: logger)
   end
 
   def self.post(uri:, headers:, body: nil, logger:)
+    request = Net::HTTP::Post.new(uri, headers)
+    request.body = body
+    _perform(http_request: request, logger: logger)
+  end
+
+  def self._perform(http_request:, logger:)
+    uri = http_request.uri
+
     logger.debug do
-      ['TUS POST',
-       request: {
-         tus_creation_url: uri.to_s,
-         header: headers,
-         body: body.to_s.truncate_middle(80) # body is usually small hash of config items
-       }]
+      header_info = {}
+      http_request.each_header do |key, value|
+        header_info[key] = value
+      end
+      request_info = { uri: uri.to_s, header: header_info }
+
+      request_body = http_request.body.to_s
+      request_info[:body_md5] = Digest::MD5.hexdigest(request_body) unless request_body.blank?
+
+      formatted_body =  case request_body.encoding
+                        when Encoding::ASCII_8BIT
+                          request_body.encoding.inspect
+                        else
+                          request_body.truncate_middle(60)
+                        end
+      request_info[:body] = formatted_body
+
+      ["TUS #{http_request.method}",
+       request: request_info]
     end
 
-    Net::HTTP.start(
+    http_response = Net::HTTP.start(
       uri.host,
       uri.port,
       use_ssl: uri.scheme == 'https'
     ) do |http|
-      http.post(uri.path, body, headers)
-    end.tap do |response|
+      http.request http_request
+    end
+
+    http_response.tap do |response|
       received_header = response.each_key.collect { |k| { k => response.header[k] } }
+
       logger.debug do
-        ['TUS POST',
+        ["TUS #{http_request.method}",
          response: {
            status: response.code,
            header: received_header,
